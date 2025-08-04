46dd5564bc4f8e5440e50cd9b347c741edc79ad9
ananiko9 <ananiko@d4e.gg>
2025-08-04 16:44:36 +0400

    add swagger and configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //get config Servcie
+  const configService = app.get(ConfigService);
+
+  app.enableCors();
+  app.use(helmet());
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Authentification API with roles')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //start the application
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
+  console.log(
+    `📚 API Documentation available at: http://localhost:${port}/api`,
+  );
 }
 bootstrap();
